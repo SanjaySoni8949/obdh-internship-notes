31989d9ea56d1b2a1717cba86b0c74b72536fe8c
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-06-26 11:43:50 +0530

    Create commands_engine.h

---
+++ ShellTask/Phase1Prototype/commands_engine.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_ENGINE_H
+#define COMMANDS_ENGINE_H
+
+void command_ping(void);
+
+void command_help(void);
+
+
+void command_status(void);
+
+void command_exit(void);
+
+
+void execute_command(char *cmd);
+#endif
