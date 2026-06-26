a9d1350433500b119c4450ef9f463e7a178883e6
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-06-26 11:43:06 +0530

    Create commands.c

---
+++ ShellTask/Phase1Prototype/commands.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <stdlib.h>   
+#include"command_engine.h"
+
+
+
+void command_ping(void){
+printf("PONG");
+}
+
+void command_help(void ){
+printf("help is on the way");
+}
+
+
+void command_status(void){
+printf("System Status:Ok");
+}
+
+void command_exit(void){
+    printf("Exiting shell...");
+
+    exit(0);// this function is standard library function stdlib used to exit the shell.
+}
+
+
+
