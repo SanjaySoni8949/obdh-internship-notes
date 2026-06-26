78a92f0024e3cf99abcc47f0562fda9bb33e5a55
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-06-26 11:42:14 +0530

    Create main.c

---
+++ ShellTask/Phase1Prototype/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <string.h>
+
+#include"command_engine.h"
+
+int  main(void){
+
+char command[50];
+while(1){
+    printf("> ");
+    fgets(command,sizeof(command),stdin);
+    command[strcspn(command, "\n")] = '\0'; // to remove the newline.
+
+      if (command[0] == '\0')// check whether the string is empty or not if empty continue.
+    {
+        continue;
+    }
+
+    execute_command(command);
+}
+
+
+    return 0;
+}
