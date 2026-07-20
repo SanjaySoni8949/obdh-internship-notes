6a69f58e7b6283720801d00a4a2974610bc383a0
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-07-20 16:29:49 +0530

    Update commands_engine.h

--- ShellTask/Phase1Prototype/commands_engine.h
+++ ShellTask/Phase1Prototype/commands_engine.h
@@ -1,22 +1,41 @@
 /*
-This file is the header file that contains the commands .
-and this file can be used by different file to access the command by just including it (  #inlcude"commands_engine.h")
-*/
+ * command_engine.h
+ *
+ * Header file for the command engine.
+ * It contains command IDs, function declarations,
+ * and the command engine interface.
+ */
+
+#ifndef COMMAND_ENGINE_H
+#define COMMAND_ENGINE_H
+
+#include <stdint.h>
+
+/* Command IDs */
+
+typedef enum
+{
+    CMD_PING   = 0x01,
+    CMD_HELP   = 0x02,
+    CMD_STATUS = 0x03,
+    CMD_EXIT   = 0x04
+
+} command_id_t;
 
 
-
-#ifndef COMMANDS_ENGINE_H
-#define COMMANDS_ENGINE_H
+/* Command Handler Prototypes */
 
 void command_ping(void);
 
 void command_help(void);
-
 
 void command_status(void);
 
 void command_exit(void);
 
 
-void execute_command(char *cmd);
+/* Command Engine */
+
+void execute_command(uint8_t cmd_id);
+
 #endif
