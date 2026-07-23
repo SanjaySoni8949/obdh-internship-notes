aebd6858ade7f907ed0fee707482d6dfa5343c36
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-07-23 13:06:21 +0530

    Refactor command_engine.h for clarity and functionality
    Updated the command engine header to improve documentation and function declarations.

--- ShellTask/Phase1Prototype/commands_engine.h
+++ ShellTask/Phase1Prototype/commands_engine.h
@@ -1,41 +1,26 @@
 /*
  * command_engine.h
  *
- * Header file for the command engine.
- * It contains command IDs, function declarations,
- * and the command engine interface.
+ * Public interface for the command engine.
+ *
+ * Declares functions used to:
+ *  - Register commands.
+ *  - Execute commands.
+ *  - Manage the shell command registry.
+ *
+ * Other modules interact with the command engine only through
+ * the functions declared in this header.
  */
+
+
+
+
 
 #ifndef COMMAND_ENGINE_H
 #define COMMAND_ENGINE_H
 
-#include <stdint.h>
+void execute_command(int argc, char *argv[]);
 
-/* Command IDs */
-
-typedef enum
-{
-    CMD_PING   = 0x01,
-    CMD_HELP   = 0x02,
-    CMD_STATUS = 0x03,
-    CMD_EXIT   = 0x04
-
-} command_id_t;
-
-
-/* Command Handler Prototypes */
-
-void command_ping(void);
-
-void command_help(void);
-
-void command_status(void);
-
-void command_exit(void);
-
-
-/* Command Engine */
-
-void execute_command(uint8_t cmd_id);
-
+void command_register(const char *name, void (*handler)(int argc, char *argv[]));
+void command_unregister_all(void);
 #endif
