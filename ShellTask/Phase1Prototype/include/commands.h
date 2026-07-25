ecea46d660977a7cb27d8b2b593f5bd493e3665d
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-07-25 09:21:08 +0530

    Update commands.h

--- ShellTask/Phase1Prototype/include/commands.h
+++ ShellTask/Phase1Prototype/include/commands.h
@@ -1,17 +1,3 @@
-/*
- * commands.h
- *
- * Public interface for all shell commands.
- *
- * Declares:
- *  - Command callback functions.
- *  - The register_commands() function used during shell
- *    initialization to register all supported commands.
- */
-
-
-
-
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
@@ -19,6 +5,9 @@ void command_ping(int argc, char *argv[]);
 void command_help(int argc, char *argv[]);
 void command_status(int argc, char *argv[]);
 void command_exit(int argc, char *argv[]);
+void command_echo(int argc , char *argv[]);
+void command_version(int argc,char*argv[]);
+
 
 void register_commands(void);
 #endif
