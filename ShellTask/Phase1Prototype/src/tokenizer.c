470c53bf7129e40017e9df5540dda0951cff839f
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-07-25 09:20:31 +0530

    Update tokenizer.c

--- ShellTask/Phase1Prototype/src/tokenizer.c
+++ ShellTask/Phase1Prototype/src/tokenizer.c
@@ -1,31 +1,3 @@
-
-/*
- * tokenizer.c
- *
- * Implements the shell tokenizer.
- *
- * Responsibilities:
- *  - Splits the input command line into individual tokens.
- *  - Converts a command string into argc/argv format.
- *  - Separates the command name from its arguments.
- *
- * Example:
- *      Input:
- *          "echo Hello World"
- *
- *      Output:
- *          argc = 3
- *          argv[0] = "echo"
- *          argv[1] = "Hello"
- *          argv[2] = "World"
- *
- * The tokenizer allows commands to receive arguments in a
- * standard command-line format.
- */
-
-
-
-
 #include<string.h>
 #include"tokenizer.h"
 
@@ -40,11 +12,11 @@ char * token;
 token=strtok( input ," ");//strtok=string token( char input , const dilimeter)  here dilimeter is space.
                           //dilimeter=A delimiter is simply a character that separates pieces of data.
 
-while(token!=NULL && *argc<max_arguments){
-    argv[*argc]=token;
+while (token != NULL && *argc < MAX_ARGUMENTS)
+{
+    argv[*argc] = token;
     (*argc)++;
 
-    token=strtok(input," ");
+    token = strtok(NULL, " ");
 }
-
 }
