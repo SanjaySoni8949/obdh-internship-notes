412fc6a1614d79111f51a3312b8d59c883b2c516
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-07-25 09:21:30 +0530

    Update commands_engine.h

--- ShellTask/Phase1Prototype/include/commands_engine.h
+++ ShellTask/Phase1Prototype/include/commands_engine.h
@@ -1,26 +1,21 @@
-/*
- * command_engine.h
- *
- * Public interface for the command engine.
- *
- * Declares functions used to:
- *  - Register commands.
- *  - Execute commands.
- *  - Manage the shell command registry.
- *
- * Other modules interact with the command engine only through
- * the functions declared in this header.
- */
-
-
-
-
-
 #ifndef COMMAND_ENGINE_H
 #define COMMAND_ENGINE_H
 
 void execute_command(int argc, char *argv[]);
 
-void command_register(const char *name, void (*handler)(int argc, char *argv[]));
+void command_register(const char *name, const char *description,const char *usage
+    ,void (*handler)(int argc, char *argv[]));
 void command_unregister_all(void);
+void command_list(void);
+typedef struct
+{
+    const char *name;
+    const char *description;
+    const char *usage;
+    void (*handler)(int argc, char *argv[]);
+} cmd_t;
+
+const cmd_t *command_find(const char *name);
+
+int command_get_count(void);
 #endif
