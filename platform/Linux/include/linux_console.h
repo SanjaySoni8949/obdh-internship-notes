6534712ce4a0492dbb5ffbf09586349f5b888158
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-08-04 14:14:42 +0530

    Update linux_console.h

--- platform/Linux/include/linux_console.h
+++ platform/Linux/include/linux_console.h
@@ -1,18 +1,6 @@
-/*
-This file tells the rest of the project:
-
-"If you want keyboard input or output, use these functions."
-*/
-
-
 #ifndef LINUX_CONSOLE_H
 #define LINUX_CONSOLE_H
 
-
-char shell_getchar(void);
-void shell_putchar(char ch);
-void shell_print(const char *str);
-
-void shell_printf(const char *fmt, ...);//... this means that it can take variable number of argument.
+#include "shell_io.h"
 
 #endif
