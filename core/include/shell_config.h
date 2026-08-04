d31b82860207c2ab9cd6d4a46d83721bddda93be
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-08-04 16:19:09 +0530

    Update shell_config.h

--- core/include/shell_config.h
+++ core/include/shell_config.h
@@ -1,7 +1,16 @@
+/*
+ * shell_config.h
+ *
+ * Compile-time configuration for the Generic Command Shell.
+ */
+
 #ifndef SHELL_CONFIG_H
 #define SHELL_CONFIG_H
 
+/** Maximum length of a command line (including the null terminator). */
 #define MAX_COMMAND_LENGTH 50
+
+/** Maximum number of arguments accepted by the tokenizer. */
 #define MAX_ARGUMENTS 10
 
-#endif
+#endif /* SHELL_CONFIG_H */
