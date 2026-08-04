a6a6eafeb5db8780a64d11bb390dafa23d9a0e28
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-08-04 16:18:51 +0530

    Update version.h

--- core/include/version.h
+++ core/include/version.h
@@ -1,16 +1,22 @@
 /*
  * version.h
  *
- * Project version information.
+ * Version information for the Generic Command Shell library.
  */
 
 #ifndef VERSION_H
 #define VERSION_H
 
+/** Major release version. */
 #define COMMAND_SHELL_VERSION_MAJOR 1
+
+/** Minor release version. */
 #define COMMAND_SHELL_VERSION_MINOR 0
+
+/** Patch release version. */
 #define COMMAND_SHELL_VERSION_PATCH 0
 
+/** Human-readable version string. */
 #define COMMAND_SHELL_VERSION_STRING "1.0.0"
 
 #endif /* VERSION_H */
