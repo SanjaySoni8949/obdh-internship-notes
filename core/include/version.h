cd509e1e9d510bc2dab8be36a9914d8e4ebddfe9
Sanjay Soni <sanjaysoni450720@gmail.com>
2026-08-04 15:01:30 +0530

    Create version.h

---
+++ core/include/version.h
@@ -0,0 +1,16 @@
+/*
+ * version.h
+ *
+ * Project version information.
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define COMMAND_SHELL_VERSION_MAJOR 1
+#define COMMAND_SHELL_VERSION_MINOR 0
+#define COMMAND_SHELL_VERSION_PATCH 0
+
+#define COMMAND_SHELL_VERSION_STRING "1.0.0"
+
+#endif /* VERSION_H */
